df2ea7d42e550d0c0be8c8ce971eb3ec0e74ab49
Ian MacIntosh <ianjmacintosh@gmail.com>
2021-01-29 15:31:34 -0300

    Include a vastly reduced version of the app

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { registerRootComponent } from 'expo';
 import { StyleSheet, Text, View } from 'react-native';
+import App from './components/App';
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Greeting test!</Text>
-      <StatusBar style="auto" />
+      <App />
     </View>
   );
 }
